cc85672a5c1b32f16c2981d136745819a5ef8c72
Matt Zelenak <matt@zelenak.me>
2021-07-08 05:11:17 -0400

    reduce noise and abstract less watch to npm script to run alongside app

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
+// node_modules
 import React from 'react'
 import ReactDOM from 'react-dom';
+
+// class components
 import Header from '../src/components/Header.js';
 import Body from '../src/components/Body.js';
 import Footer from '../src/components/Footer.js';
 
-import './main.less';
-
-const layout = (
-	<div id='flex-container'>
-		<Header
-			logo='https://png.pngtree.com/png-clipart/20190611/original/pngtree-wolf-logo-png-image_2306634.jpg'
-			name='Fire Tigers' />
-		<Body />
-		<Footer />
-	</div>
-);
-
 ReactDOM.render(
-	layout,
+	(
+		<div id='flex-container'>
+			<Header
+				logo='https://png.pngtree.com/png-clipart/20190611/original/pngtree-wolf-logo-png-image_2306634.jpg'
+				name='Fire Tigers' />
+			<Body />
+			<Footer />
+		</div>
+	),
 	document.getElementById('root')
 );
