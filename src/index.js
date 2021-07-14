d4bb22c572faa42d494decacc76c75e777a8db11
Matt Zelenak <matt@zelenak.me>
2021-07-14 13:36:43 -0400

    compile less outside of webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import App from './/components/App.js';
-import './main.less';
+import './main.css';
 
 if (module.hot) module.hot.accept();
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,51 @@
 const path = require('path');
 const webpack = require('webpack');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const config = {
-	mode: 'development',
-	entry: path.resolve(__dirname, 'src', 'index.js'),
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	devServer: {
-		hot: true,
-		open: true,
-		port: 1080,
-		clientLogLevel: 'silent',
-		contentBase: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(jsx|js)$/,
-				include: path.resolve(__dirname, 'src'),
-				exclude: /node_modules/,
-				use: [{
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							['@babel/preset-env', {
-								"targets": "defaults" 
-							}],
-							'@babel/preset-react'
-						]
-					}
-				}]
-			},
-			{ 
-                test: /\.less$/,
-                use: [ 
-                    'style-loader',
-                    'css-loader', 
-                    'less-loader'
-                ]
-            }
-		]
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	],
-	// plugins: [
-	// 	new HtmlWebpackPlugin({
-	// 		title: 'Development',
-	// 		template: path.resolve(__dirname, 'dist', 'index.html'),
-	// 	}),
-	// ],
+module.exports = env => {
+	return {
+		mode: 'development',
+		entry: path.resolve(__dirname, 'src', 'index.js'),
+		output: {
+			filename: 'bundle.js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+		devServer: {
+			hot: true,
+			open: true,
+			port: 1080,
+			clientLogLevel: 'silent',
+			contentBase: path.resolve(__dirname, 'dist'),
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: '[name].bundle.css',
+			}),
+			new webpack.HotModuleReplacementPlugin(),
+		],
+		module: {
+			rules: [
+				{
+					test: /\.css$/i,
+					use: [MiniCssExtractPlugin.loader, 'css-loader']
+				},
+				{
+					test: /\.(jsx|js)$/,
+					include: path.resolve(__dirname, 'src'),
+					exclude: /node_modules/,
+					use: [{
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								['@babel/preset-env', {
+									"targets": "defaults" 
+								}],
+								'@babel/preset-react'
+							]
+						}
+					}]
+				},
+			]
+		},
+	};
 };
-
-module.exports = config;
