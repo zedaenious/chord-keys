6672ad7a12124a13f6c4cead802f53a0dd9cfa7a
Matt Zelenak <matt@zelenak.me>
2021-07-14 12:05:00 -0400

    cleaning up deps and scripts

--- webpack.config.js
+++ webpack.config.js
@@ -11,15 +11,20 @@ const config = {
 	module: {
 		rules: [
 			{
-				test: /\.jsx?$/,
+				test: /\.(jsx|js)$/,
+				include: path.resolve(__dirname, 'src'),
 				exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					presets: [
-						'@babel/preset-env',
-						'@babel/preset-react',
-					],
-				},
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							['@babel/preset-env', {
+								"targets": "defaults" 
+							}],
+							'@babel/preset-react'
+						]
+					}
+				}]
 			},
 			{ 
                 test: /\.less$/,
@@ -27,22 +32,14 @@ const config = {
                     'style-loader',
                     'css-loader', 
                     'less-loader'
-                ],
-            },
-			{
-				test: /\.(png|jpe?g|gif)$/i,
-				use: [
-					{
-						loader: 'file-loader'
-					}
-				]
-			}
-		],
+                ]
+            }
+		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: 'Development',
-			template: path.resolve(__dirname, 'src', 'index.html'),
+			template: path.resolve(__dirname, 'dist', 'index.html'),
 		}),
 	],
 };
